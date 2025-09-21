263fad67eb1afe481ff82f656735ec8bb9c09694
vishalbh93 <167605096+vishalbh93@users.noreply.github.com>
2025-09-21 12:19:36 +0530

    Added updated files

---
+++ Service.WebAPI/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service.Generativelogic.Interfaces;
+using Service.Models;
+
+namespace Servie.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        #region Public Variables
+        private readonly IAccountService _dataservice;
+        #endregion
+
+        public AccountController(IAccountService dataservice)
+        {
+            _dataservice = dataservice;
+        }
+
+        #region Public Methods
+        [HttpPost]
+        public ActionResult Login([FromBody] LoginDetails data)
+        {
+            var result = _dataservice.AuthenticateUse(data?.UserName ?? string.Empty, data?.Password ?? string.Empty);
+            if (result != null)
+            {
+                return Ok(_dataservice.GenerateToken(result));
+            }
+
+            return Unauthorized();
+        }
+
+        #endregion
+    }
+}
---
+++ Service.WebAPI/RegisterServices.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Service.Generativelogic.AccountService;
+using Service.Generativelogic.Interfaces;
+using System.Text;
+
+namespace Servie.WebAPI
+{
+    public class RegisterServices
+    {
+        #region Public Methods
+        public static void RegisterComponent(WebApplicationBuilder builder)
+        {
+            RegisterDataService(builder.Services, builder.Configuration);
+            RegisterJWTService(builder);
+            RegisterCORSservice(builder);
+            builder.Services.AddControllers();
+            builder.Services.AddAuthorization();
+        }
+        #endregion
+
+        #region Private Methods
+        private static void RegisterDataService(IServiceCollection services, IConfiguration configuration)
+        {
+            //services.AddScoped<IDataService>(s =>new DataService());
+            services.AddScoped<IAccountService>(s => new AccountService(configuration));
+        }
+
+        private static void RegisterJWTService(WebApplicationBuilder builder)
+        {
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                        ValidAudience = builder.Configuration["Jwt:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+                        ClockSkew = TimeSpan.Zero
+                    };
+            });
+        }
+
+        private static void RegisterCORSservice(WebApplicationBuilder builder)
+        {
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOrigin",
+                    builder =>
+                    {
+                        builder.WithOrigins("http://localhost:3000")
+                               .AllowAnyHeader()
+                               .AllowAnyMethod()
+                               .AllowCredentials();
+                    });
+            });
+        }
+
+        #endregion
+    }
+}
